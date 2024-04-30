c0e64e567c3c16200d7dbdc343873a073778730f
Ayush <me21b1076@iiitdm.ac.in>
2024-04-30 06:07:58 +0530

    Sign Up Page

---
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -0,0 +1,20 @@
+const GenderBox = () => {
+    return (
+        <div className="flex">
+            <div className="form-control">
+                <label className={`label gap-2 cursor-pointer`}>
+                    <span className="text-label">Male</span>
+                    <input type="checkbox" className="checkbox border-slate-900" />
+                </label>
+            </div>
+            <div className="form-control">
+                <label className={`label gap-2 cursor-pointer`}>
+                    <span className="text-label">Female</span>
+                    <input type="checkbox" className="checkbox border-slate-900" />
+                </label>
+            </div>
+        </div>
+    )
+}
+
+export default GenderBox
