8a9199be9f23da4e84acdbbf3135affedc7cd48f
Ayush <me21b1076@iiitdm.ac.in>
2024-04-30 19:24:03 +0530

    Messages view

---
+++ frontend/src/components/Messages/Messages.jsx
@@ -0,0 +1,19 @@
+import SingleMessage from "./SingleMessage";
+
+const Messages = () =>{
+    return <>
+        <div className="px-4 flex-1 overflow-auto">
+            <SingleMessage />
+            <SingleMessage />
+            <SingleMessage />
+            <SingleMessage />
+            <SingleMessage />
+            <SingleMessage />
+            <SingleMessage />
+            <SingleMessage />
+            <SingleMessage />
+        </div>
+    </>;
+}
+
+export default Messages
