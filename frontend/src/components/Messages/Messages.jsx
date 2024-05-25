d6d39d665b6311e6c7ae8545d25a96925de95109
Ayush <me21b1076@iiitdm.ac.in>
2024-05-26 01:24:09 +0530

    Get Message Functionality

--- frontend/src/components/Messages/Messages.jsx
+++ frontend/src/components/Messages/Messages.jsx
@@ -1,17 +1,27 @@
+import { useEffect, useRef } from "react";
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/messageSkeleton";
 import SingleMessage from "./SingleMessage";
 
-const Messages = () =>{
+const Messages = () => {
+    const { messages, loading } = useGetMessages()
+    console.log("Messages : ", messages)
+    const lastMessageRef = useRef()
+    useEffect(() => {
+        setTimeout(()=>{
+            lastMessageRef.current?.scrollIntoView({behavior: 'smooth'})
+        }, 100)
+    },[messages])
+
     return <>
         <div className="px-4 flex-1 overflow-auto">
-            <SingleMessage />
-            <SingleMessage />
-            <SingleMessage />
-            <SingleMessage />
-            <SingleMessage />
-            <SingleMessage />
-            <SingleMessage />
-            <SingleMessage />
-            <SingleMessage />
+            {!loading && messages.length > 0 && messages.map((message) => {
+                <div key={message._id} ref={lastMessageRef}><SingleMessage message={message.message} /></div>
+            })}
+            {loading && [...Array(3)].map((_, idx) => { <MessageSkeleton key={idx} /> })}
+            {!loading && messages.length === 0 && (
+                <p className="text-center">Send a message to start Conversation</p>
+            )}
         </div>
     </>;
 }
--- frontend/src/components/Messages/SingleMessage.jsx
+++ frontend/src/components/Messages/SingleMessage.jsx
@@ -1,19 +1,30 @@
-const SingleMessage = () => {
+import {useAuthContext} from '../../context/auth.context'
+import { extractTime } from '../../utils/extractTime';
+import useConversation from '../../zustand/useConversation';
+
+const SingleMessage = ({message}) => {
+    const {authUser} = useAuthContext()
+    const {selectedConversation} = useConversation()
+    const fromMe = message.senderId === authUser._id 
+    const formattedTime = extractTime(message.createdAt)
+    const chatClassName = fromMe ? 'chat-end' : 'chat-start';
+    const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+    const bubbleBgColor = fromMe ? 'bg-blue-500' : '';
     return <>
-        <div className="chat chat-end ">
+        <div className={`chat ${chatClassName}`}>
             <div className="chat-image avatar">
                 <div className="w-10 rounded-full">
                     <img
-                        src="https://avatar.iran.liara.run/public/boy?username=Ayush"
+                        src={profilePic}
                         alt="User DP Avatar"
                     />
                 </div>
             </div>
-            <div className="chat-bubble text-white bg-blue-500">
-                Hii! What's Up? This Is Ayush
+            <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>
+                {message.message}
             </div>
             <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
-                19:42
+                {formattedTime}
             </div>
             
         </div>
